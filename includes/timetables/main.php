51f366874f4abd6c59f8f265eb894ed06f3305cd
gpuhalev <gesho.puh@gmail.com>
2017-05-04 14:16:25 +0200

    adding timetable file

---
+++ includes/timetables/main.php
@@ -0,0 +1,10 @@
+<?php
+	public static $sofia = "14:00";
+	public static $budapest = "02:15";
+	public static $banskabistrica = "04:30";
+	public static $krakow = "10:00";
+	public static $katowice = "12:00";
+	public static $czestochowa = "13:30";
+	public static $ludz = "15:30";
+	public static $warszawa = "17:30";
+?>
